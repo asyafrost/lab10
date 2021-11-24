325d38014874ef8974f95ae001f2d6100cfe4069
asyafrost <52134745+asyafrost@users.noreply.github.com>
2021-11-24 13:48:32 +0300

    Add files via upload

---
+++ lab101.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "");
+	int a, b;
+	cout << "Введите два числа А и В:" << endl;
+	cin >> a >> b;
+	if (a > 2 && b <= 3)
+	{
+		cout << "Справедливы неравенства А>2 и B<=3";
+	}
+	else
+	{
+		cout << "Высказывание неверно";
+	}
+	return 0;
+}
---
+++ lab102.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "");
+	int a, b, c;
+	cout << "Введите три целых числа (А, В, С):" << endl;
+	cin >> a >> b >> c;
+	if (a < b && b < c)
+	{
+		cout << "Справедливо двойное неравенство A < B < C";
+	}
+	else
+	{
+		cout << "Выражение A < B < C неверно";
+	}
+	return 0;
+}
